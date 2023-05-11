2076eed83b9c6d3ee99792dff95dd37081542a4b
EC2 Default User <ec2-user@ip-172-31-26-149.ap-northeast-1.compute.internal>
2023-05-11 04:58:55 +0000

    [Add]ルートパス

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  root to: 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
